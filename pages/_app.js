a3d64a048ad10730c74a8457f5abefc55fe71acc
aimalkhanofficial <aimalkhanofficial@gmail.com>
2023-10-01 08:20:19 -0400

    Added a basic menu layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import Link from 'next/link'
+
+function MenuLayout () {
+  return (
+    <ul>
+      <Link href='./'>Home | </Link>
+      <Link href='./contacts'>Contacts | </Link>
+    </ul>
+  )
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <MenuLayout/>
+      <br/>
+      <Component {...pageProps} />
+    </>
+  )
 }
