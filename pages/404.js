5d1d4366bf7adfc924dd01a3a64d8fcb6a81c91e
aimalkhanofficial <aimalkhanofficial@gmail.com>
2023-10-08 09:01:31 -0400

    Added a NotFound image to try it out with next/image

--- pages/404.js
+++ pages/404.js
@@ -1,13 +1,21 @@
 import Layout from "@/components/Layout";
-
+import Image from "next/image";
+import NotFoundImage from '@/public/not-found.gif'
 export default function ErrPage() {
     return (
         <Layout>
             <p>Page Not found</p>
+            <div className={'imageStyle'} ><Image src={NotFoundImage} alt="NotFound"/></div>
             <style jsx>
                 {`
+                    .imageStyle {
+                        margin-left: auto;
+                        margin-right: auto;
+                        width: 20%;
+                    }
                     p {
                         color: red;
+                        display: none;
                     }
                 `}
             </style>
